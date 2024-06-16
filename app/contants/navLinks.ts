d45aae01b6a8d0a2f073534dcb17d435bb04e6cc
Floriane Grosset <fl.grosset@gmail.com>
2024-06-16 13:57:28 +0200

    ✨ add about page

--- app/contants/navLinks.ts
+++ app/contants/navLinks.ts
@@ -2,3 +2,5 @@ export const NAV_LINKS = [
   { label: 'About', url: '/' },
   { label: 'Contact', url: '/contact' },
 ]
+export const SITE_TAGELINE =
+  'I’m Flo and I am a fullstack web dev, passionate about accessibility, sustainability, and art'
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
-import type { Metadata } from "next";
-import { Counter } from "./components/counter/Counter";
+import { Typography } from '@mui/material'
+import Link from './components/link/Link'
+import LayoutColumn from './components/layout/LayoutColumn'
+import MainTitle from './components/mainTitle/MainTitle'
+import { SITE_TAGELINE } from './contants/navLinks'
 
 export default function IndexPage() {
-  return <Counter />;
+  return (
+    <>
+      <LayoutColumn isSticky stickyTopPosition={70}>
+        <MainTitle title="About" description={SITE_TAGELINE} lang="fr">
+          Bonjour !
+        </MainTitle>
+        <Typography variant="caption" mb={6}>
+          {SITE_TAGELINE}
+        </Typography>
+      </LayoutColumn>
+      <LayoutColumn marginTop={40}>
+        <Typography mb={3}>
+          I am originally French, and now living in Hamburg, Germany.
+        </Typography>
+
+        <Typography mb={3}>
+          I am the founder of{' '}
+          <Link href="https://waawgallery.com" isExternal>
+            <span aria-label="waaw">WAAW</span>, a non-profit online gallery
+          </Link>
+          . We aim to promote and help the creation of quality web-based
+          artworks.
+        </Typography>
+        <Typography mb={3}>
+          From February 2022 to July 2023, I was working as a frontend engineer
+          at{' '}
+          <Link href="https://getground.co.uk" isExternal>
+            GetGround
+          </Link>
+          .
+        </Typography>
+        <Typography mb={3}>
+          I love working with Javascript, React, Redux and Typescript, while
+          caring for semantic HTML, accessibility, and good user experience.
+        </Typography>
+      </LayoutColumn>
+    </>
+  )
 }
 
 export const metadata: Metadata = {
